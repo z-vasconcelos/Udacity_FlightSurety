95a25a11b3fd8cc4451555a3b7be22db9d382764
Jose Carlos Vasconcelos Júnior <jose.vasconcelos.zv@gmail.com>
2021-12-25 11:37:34 -0300

    init

---
+++ truffle-config.js
@@ -0,0 +1,23 @@
+var HDWalletProvider = require("@truffle/hdwallet-provider");
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/v3/b3d19109d43642a5a916742f919c0081'),
+        network_id: 4,
+        gas: 4500000,       
+        gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24"
+    }
+  }
+};
